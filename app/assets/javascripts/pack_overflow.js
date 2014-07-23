a43be953ac580c8953a7b1e84694f3cf739da78d
App Academy Student <student@appacademy.io>
2014-07-23 13:42:03 -0700

    basic rails auth stuff

---
+++ app/assets/javascripts/pack_overflow.js
@@ -0,0 +1,13 @@
+window.PackOverflow = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    
+  }
+};
+
+$(document).ready(function(){
+  PackOverflow.initialize();
+});
