6f6713c14240a7efdd0757e6ea2681be578dcfc6
App Academy Student <student@appacademy.io>
2014-07-28 16:28:04 -0700

    index and show queries fixed

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -20,14 +20,24 @@ module Api
 
     def index
       #should return top questions or whatever eventually
-      @questions = Question.select("questions.*, SUM(votes.value) AS sumVotes").joins(:votes).group("questions.id")
+      @questions = Question.select("questions.*, SUM(votes.value) AS sumVotes")
+            .joins("LEFT OUTER JOIN votes ON (votes.votable_id = questions.id AND votes.votable_type = 'Question')").group("questions.id");
       render json: @questions
     end
 
     def show
-      @question = Question.select("questions.*, SUM(votes.value) AS sumVotes").joins(:votes).group("questions.id").find(params[:id]);
-      @answers = Answer.select("answers.*, SUM(votes.value) AS sumVotes").joins("LEFT OUTER JOIN votes ON (votes.votable_id = answers.id AND votes.votable_type = 'Answer')").group("answers.id").where(question_id: params[:id]);
-      @comments = Comment.select("comments.*, SUM(votes.value) AS sumVotes").joins("LEFT OUTER JOIN votes ON (votes.votable_id = comments.id AND votes.votable_type = 'Comment')").group("comments.id").where( "(commentable_id = ? AND commentable_type = 'Question') OR (commentable_id IN (?) AND commentable_type = 'Answer')", @question.id, @answers.map(&:id));
+      # @question = Question.select("questions.*, SUM(votes.value) AS sumVotes")
+  #           .joins(:votes).group("questions.id").find(params[:id]);
+      @question = Question.select("questions.*, SUM(votes.value) AS sumVotes")
+            .joins("LEFT OUTER JOIN votes ON (votes.votable_id = questions.id AND votes.votable_type = 'Question')").group("questions.id").find(params[:id])
+      @answers = Answer.select("answers.*, SUM(votes.value) AS sumVotes")
+            .joins("LEFT OUTER JOIN votes ON (votes.votable_id = answers.id AND votes.votable_type = 'Answer')")
+            .group("answers.id").where(question_id: params[:id]);
+      @comments = Comment.select("comments.*, SUM(votes.value) AS sumVotes")
+            .joins("LEFT OUTER JOIN votes ON (votes.votable_id = comments.id AND votes.votable_type = 'Comment')")
+            .group("comments.id").where( "(commentable_id = ? AND commentable_type = 'Question') OR (commentable_id IN (?) AND commentable_type = 'Answer')", 
+            @question.id, @answers.map(&:id));
+            
       if @question
         render :show
       else
