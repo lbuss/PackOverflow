c6e6db25ded63ad7fb25be73529db76ee5033bd4
App Academy Student <student@appacademy.io>
2014-08-11 15:11:27 -0700

    guest sign in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,4 @@
 class StaticPagesController < ApplicationController
   def root
-    if !current_user
-      @user = User.find_by_username_and_password(username: "Guest", password: "password")
-      sign_in!(@user)
-      redirect_to root_url
-    else
-      flash.now[:errors] = ["invalid email and/or password"]
-      render :new
-    end
   end
 end
