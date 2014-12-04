1c4991ae8ed49417bccbf7b4aa0f5f57ba781565
Lawrence Buss <LawSBuss@gmail.com>
2014-12-03 17:23:31 -0800

    slight form css change

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def root
-    if !current_user
-      redirect_to new_session_url
-    end
+    # if !current_user
+    #   redirect_to new_session_url
+    # end
   end
 end
